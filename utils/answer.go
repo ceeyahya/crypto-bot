8e3c64bc96c776fa84a1c30dd4e51432dd2a23bf
Yahya <chahineyahya@pm.me>
2019-08-29 02:44:53 +0100

    Added README

--- utils/answer.go
+++ utils/answer.go
@@ -18,6 +18,9 @@ func Answer(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
 	if len(txt) > 5 {
 		rsp = "I don't think that's a Cryptocurrency try something along the lines of 'BTC' or 'ETH'"
 		rtm.SendMessage(rtm.NewOutgoingMessage(rsp, msg.Channel))
+	} else if txt == "help" {
+		rsp = "Hey There, I'm CryptoBot a nice .. Bot. You can tag me on any channel and add a cryptocurrency symbol and I'll get you the rates. Try me, I'm the best at what I do."
+		rtm.SendMessage(rtm.NewOutgoingMessage(rsp, msg.Channel))
 	} else {
 		resp := handlers.GetCurrency(txt)
 		rtm.SendMessage(rtm.NewOutgoingMessage(txt+" is estimated at "+resp+" $", msg.Channel))
